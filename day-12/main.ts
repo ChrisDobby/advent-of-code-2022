3fbef4345b6c9c74c5ec0079a1d12a2f9af65828
Chris Dobson <chris.dobson@dazn.com>
2022-12-12 20:58:22 +0000

    renaming

--- day-12/main.ts
+++ day-12/main.ts
@@ -3,10 +3,10 @@ type Point = [number, number]
 
 const getElevationValue = (value: string) => value.replace('S', 'a').replace('E', 'z').charCodeAt(0) - 97
 
-const getPointsWithValue = (grid: HeightMap, value: string): Point[] =>
-  grid.flatMap((row, rowIndex) => row.flatMap((col, colIndex) => (col === value ? [[rowIndex, colIndex] as Point] : [])))
+const getPointsWithValue = (heightMap: HeightMap, value: string): Point[] =>
+  heightMap.flatMap((row, rowIndex) => row.flatMap((col, colIndex) => (col === value ? [[rowIndex, colIndex] as Point] : [])))
 
-const getElevationValueAtPoint = (grid: HeightMap, point: Point) => getElevationValue(grid[point[0]][point[1]])
+const getElevationValueAtPoint = (heightMap: HeightMap, point: Point) => getElevationValue(heightMap[point[0]][point[1]])
 
 const getMinimumStepsForPoints = (queueItems: [Point, number][]): [Point, number][] =>
   queueItems.reduce((acc, [point, steps]) => {
@@ -16,7 +16,7 @@ const getMinimumStepsForPoints = (queueItems: [Point, number][]): [Point, number
     return acc
   }, [] as [Point, number][])
 
-const getValidNeighbours = (grid: HeightMap, point: Point): Point[] =>
+const getValidNeighbours = (heightMap: HeightMap, point: Point): Point[] =>
   (
     [
       [point[0] - 1, point[1]],
@@ -25,10 +25,10 @@ const getValidNeighbours = (grid: HeightMap, point: Point): Point[] =>
       [point[0], point[1] + 1],
     ] as Point[]
   )
-    .filter(([row, col]) => row >= 0 && col >= 0 && row < grid.length && col < grid[0].length)
-    .filter(neighbour => getElevationValueAtPoint(grid, neighbour) <= getElevationValueAtPoint(grid, point) + 1)
+    .filter(([row, col]) => row >= 0 && col >= 0 && row < heightMap.length && col < heightMap[0].length)
+    .filter(neighbour => getElevationValueAtPoint(heightMap, neighbour) <= getElevationValueAtPoint(heightMap, point) + 1)
 
-const breadthFirstSearch = (grid: HeightMap, finish: Point, queue: [Point, number][], alreadyVisited: string[] = []): number => {
+const breadthFirstSearch = (heightMap: HeightMap, finish: Point, queue: [Point, number][], alreadyVisited: string[] = []): number => {
   const stepsToFinish = queue
     .filter(([point, _]) => point[0] === finish[0] && point[1] === finish[1])
     .map(([_, steps]) => steps)
@@ -38,24 +38,26 @@ const breadthFirstSearch = (grid: HeightMap, finish: Point, queue: [Point, numbe
   const visited = new Set([...alreadyVisited, ...queue.map(([point, _]) => point.toString())])
   const updatedQueue = getMinimumStepsForPoints(
     queue.flatMap(([queuePoint, steps]) =>
-      getValidNeighbours(grid, queuePoint)
+      getValidNeighbours(heightMap, queuePoint)
         .filter(point => !visited.has(point.toString()))
         .map(point => [point, steps + 1] as [Point, number])
     )
   )
 
-  return breadthFirstSearch(grid, finish, updatedQueue, [...visited])
+  return breadthFirstSearch(heightMap, finish, updatedQueue, [...visited])
 }
 
-const input = Deno.readTextFileSync('./input.txt').split('\n').filter(Boolean)
-const grid = input.map(line => line.split(''))
-const startingPoint = getPointsWithValue(grid, 'S')[0]
-const finishingPoint = getPointsWithValue(grid, 'E')[0]
-const aPoints = [startingPoint, ...getPointsWithValue(grid, 'a')]
+const heightMap = Deno.readTextFileSync('./input.txt')
+  .split('\n')
+  .filter(Boolean)
+  .map(line => line.split(''))
+const startingPoint = getPointsWithValue(heightMap, 'S')[0]
+const finishingPoint = getPointsWithValue(heightMap, 'E')[0]
+const aPoints = [startingPoint, ...getPointsWithValue(heightMap, 'a')]
 
-const numberOfStepsFromStart = breadthFirstSearch(grid, finishingPoint, [[startingPoint, 0]])
+const numberOfStepsFromStart = breadthFirstSearch(heightMap, finishingPoint, [[startingPoint, 0]])
 const shortestRouteFromA = breadthFirstSearch(
-  grid,
+  heightMap,
   finishingPoint,
   aPoints.map(point => [point, 0])
 )
